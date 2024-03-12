0ea017e43690bd1d3d5d494083aad35d92786fcf
Didoqkiq <flydidobg@gmail.com>
2024-03-12 16:44:20 +0200

    MVC Structure added

---
+++ HouseRentingSystem/Controllers/AgentController.cs
@@ -0,0 +1,23 @@
+﻿using HouseRentingSystem.Core.Models.Agent;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    [Authorize]
+    public class AgentController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Become()
+        {
+            var model = new BecomeAgentFromModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Become(BecomeAgentFromModel model)
+        {
+            return RedirectToAction(nameof(HouseController.All), "House");
+        }
+    }
+}
