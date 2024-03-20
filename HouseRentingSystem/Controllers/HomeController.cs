6ad5b5235a479a2961978e8d7e85df9871a67aa2
Didoqkiq <flydidobg@gmail.com>
2024-03-20 13:34:16 +0200

    BaseController added

--- HouseRentingSystem/Controllers/HomeController.cs
+++ HouseRentingSystem/Controllers/HomeController.cs
@@ -1,12 +1,12 @@
 ﻿using HouseRentingSystem.Core.Contracts;
-using HouseRentingSystem.Core.Models.Home;
 using HouseRentingSystem.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace HouseRentingSystem.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> logger;
         private readonly IHouseService houseService;
@@ -18,6 +18,7 @@ namespace HouseRentingSystem.Controllers
             houseService = _houseService;
         }
 
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             var model = await houseService.LastThreeHouses();
@@ -25,6 +26,7 @@ namespace HouseRentingSystem.Controllers
             return View(model);
         }
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
