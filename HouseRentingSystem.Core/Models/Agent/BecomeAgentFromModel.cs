3d03ce3df41313907820ae5b93ea0f5a9b36fb0f
Didoqkiq <flydidobg@gmail.com>
2024-03-21 14:00:47 +0200

    BecomeAgentModel Created

--- HouseRentingSystem.Core/Models/Agent/BecomeAgentFromModel.cs
+++ HouseRentingSystem.Core/Models/Agent/BecomeAgentFromModel.cs
@@ -1,6 +1,16 @@
-﻿namespace HouseRentingSystem.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystem.Core.Models.Agent
 {
     public class BecomeAgentFromModel
     {
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(AgentPhoneNumberMaxLength, MinimumLength = AgentPhoneNumberMinLength, ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = null!;
     }
 }
