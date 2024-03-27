1a1bf78b0baa2a2bc5013ded3bf50c775a46aeab
Didoqkiq <flydidobg@gmail.com>
2024-03-27 13:04:58 +0200

    Add House implemented

--- HouseRentingSystem.Core/Contracts/IHouseService.cs
+++ HouseRentingSystem.Core/Contracts/IHouseService.cs
@@ -1,4 +1,5 @@
 ﻿using HouseRentingSystem.Core.Models.Home;
+using HouseRentingSystem.Core.Models.House;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,12 @@ namespace HouseRentingSystem.Core.Contracts
 {
     public interface IHouseService
     {
-        public Task<IEnumerable<HouseIndexServiceModel>> LastThreeHousesAsync();
+        Task<IEnumerable<HouseIndexServiceModel>> LastThreeHousesAsync();
+
+        Task<IEnumerable<HouseCategoryServiceModel>> AllCategoriesAsync();
+
+        Task<bool> CategoryExistsAsync(int categoryId);
+
+        Task<int> CreateAsync(HouseFormModel model, int agentId);
     }
 }
